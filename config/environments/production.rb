9b293318863c2d2c815273d4644b070d1ebc8840
Ioannis-Andreas Philippas <iphilippas@gmail.com>
2015-04-08 15:46:33 +0300

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
